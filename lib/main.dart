d20158a2e4db0969b31cd8ca89d171c81257d20b
Sameer <sameer.q08@gmail.com>
2023-09-11 14:48:44 +0530

    "Stack"

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   int x = 0;
 
   @override
@@ -16,10 +21,29 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('AppBar'),
         ),
-        body: SafeArea(child: Center(child: Text(x.toString()))),
+        body: Column(
+          children: [
+            SafeArea(child: Center(child: Text(x.toString()))),
+            Stack(children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+                child: const Text('i am container 1'),
+              ),
+              Container(
+                color: Colors.green,
+                width: 50,
+                height: 50,
+                child: const Text('i am container 1'),
+              ),
+            ]),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             x++;
+            setState(() {});
           },
           child: const Icon(Icons.add),
         ),
